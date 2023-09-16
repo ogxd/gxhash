1a5fe9936940a0f7b4ddb64c681658aa1ce31e2d
Olivier Giniaux <oginiaux@gmail.com>
2023-09-16 20:50:07 +0200

    Improve ILP benchmark

--- benches/ilp_sum.rs
+++ benches/ilp_sum.rs
@@ -1,63 +1,86 @@
+
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
-const ITERATIONS: u32 = 100000;
-
-fn sum_naive() -> u32 {
-    let mut sum = 0;
-    for i in 0..ITERATIONS {
-        sum = black_box(sum + 1);
-    }
-    sum
+#[inline]
+fn dohash(hash: usize, value: usize) -> usize {
+    hash * value + value
 }
 
-fn sum_unrolled() -> u32 {
-    let mut sum = 0;
-    for i in 0..ITERATIONS/5 {
-        sum = black_box(sum + 1);
-        sum = black_box(sum + 1);
-        sum = black_box(sum + 1);
-        sum = black_box(sum + 1);
-        sum = black_box(sum + 1);
+fn simple_loop(input: &[usize]) -> usize {
+    let mut hash = 0;
+    let mut i: usize = 0;
+    while i < input.len() {
+        hash = dohash(hash, input[i]);
+
+        i = i + 1;
     }
-    sum
+    hash
 }
 
-fn sum_unrolled_tempsum() -> u32 {
-    let mut sum = 0;
-    for i in 0..ITERATIONS/5 {
-        let mut temp_sum: u32 = 0;
-        temp_sum = black_box(temp_sum + 1);
-        temp_sum = black_box(temp_sum + 1);
-        temp_sum = black_box(temp_sum + 1);
-        temp_sum = black_box(temp_sum + 1);
-        temp_sum = black_box(temp_sum + 1);
+fn unrolled_loop(input: &[usize]) -> usize {
+    let mut hash: usize = 0;
+    let mut i: usize = 0;
+    while i < input.len() {
+        hash = dohash(hash, input[i]);
+        hash = dohash(hash, input[i + 1]);
+        hash = dohash(hash, input[i + 2]);
+        hash = dohash(hash, input[i + 3]);
+        hash = dohash(hash, input[i + 4]);
 
-        sum = black_box(sum + temp_sum);
+        i = i + 5;
     }
-    sum
+    hash
 }
 
-fn sum_unrolled_5_lanes() -> u32 {
-    let mut sum1 = 0;
-    let mut sum2 = 0;
-    let mut sum3 = 0;
-    let mut sum4 = 0;
-    let mut sum5 = 0;
-    for i in 0..ITERATIONS/5 {
-        sum1 = black_box(sum1 + 1);
-        sum2 = black_box(sum2 + 1);
-        sum3 = black_box(sum3 + 1);
-        sum4 = black_box(sum4 + 1);
-        sum5 = black_box(sum5 + 1);
+fn unrolled_loop_temp_per_iteration(input: &[usize]) -> usize {
+    let mut hash: usize = 0;
+    let mut i: usize = 0;
+    while i < input.len() {
+        let mut temp_hash: usize = 0;
+        temp_hash = dohash(temp_hash, input[i]);
+        temp_hash = dohash(temp_hash, input[i + 1]);
+        temp_hash = dohash(temp_hash, input[i + 2]);
+        temp_hash = dohash(temp_hash, input[i + 3]);
+        temp_hash = dohash(temp_hash, input[i + 4]);
+
+        hash = dohash(hash, temp_hash);
+
+        i = i + 5;
     }
-    black_box(sum1 + sum2 + sum3 + sum4 + sum5)
+    hash
 }
+
+fn unrolled_laned_loop(input: &[usize]) -> usize {
+    let mut hash1: usize = 0;
+    let mut hash2: usize = 0;
+    let mut hash3: usize = 0;
+    let mut hash4: usize = 0;
+    let mut hash5: usize = 0; 
+    let mut i: usize = 0;
+    while i < input.len() {
+        hash1 = dohash(hash1, input[i]);
+        hash2 = dohash(hash2, input[i + 1]);
+        hash3 = dohash(hash3, input[i + 2]);
+        hash4 = dohash(hash4, input[i + 3]);
+        hash5 = dohash(hash5, input[i + 4]);
+
+        i = i + 5;
+    }
+    dohash(dohash(dohash(dohash(hash1, hash2), hash3), hash4), hash5)
+}
+
+use rand::Rng;
 
 fn ilp_benchmark(c: &mut Criterion) {
-    c.bench_function("sum_naive", |b| b.iter(|| black_box(sum_naive())));
-    c.bench_function("sum_unrolled", |b| b.iter(|| black_box(sum_unrolled())));
-    c.bench_function("sum_unrolled_tempsum", |b| b.iter(|| black_box(sum_unrolled_tempsum())));
-    c.bench_function("sum_unrolled_5_lanes", |b| b.iter(|| black_box(sum_unrolled_5_lanes())));
+    let mut rng = rand::thread_rng();
+    let mut input: [usize; 100000] = [0; 100000];
+    for i in 0..input.len() {
+        input[i] = rng.gen::<usize>();
+    }
+    c.bench_function("simple_loop", |b| b.iter(|| black_box(simple_loop(&input))));
+    c.bench_function("unrolled_loop", |b| b.iter(|| black_box(unrolled_loop(&input))));
+    c.bench_function("unrolled_loop_temp_per_iteration", |b| b.iter(|| black_box(unrolled_loop_temp_per_iteration(&input))));
+    c.bench_function("unrolled_laned_loop", |b| b.iter(|| black_box(unrolled_laned_loop(&input))));
 }
 
 criterion_group!(benches, ilp_benchmark);
