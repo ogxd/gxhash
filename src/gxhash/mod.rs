54e143811c076d41bfff78579fb7e80890718bf1
Olivier Giniaux <oginiaux@smartadserver.com>
2023-10-22 16:48:14 +0200

    Go for inline fallthrough

--- src/gxhash/mod.rs
+++ src/gxhash/mod.rs
@@ -4,7 +4,7 @@ mod platform;
 
 pub use platform::*;
 
-#[inline] // To be disabled when profiling
+#[inline(always)] // To be disabled when profiling
 pub fn gxhash0_32(input: &[u8], seed: i32) -> u32 {
     unsafe {
         let p = &gxhash::<0>(input, seed) as *const state as *const u32;
@@ -12,7 +12,7 @@ pub fn gxhash0_32(input: &[u8], seed: i32) -> u32 {
     }
 }
 
-#[inline] // To be disabled when profiling
+#[inline(always)] // To be disabled when profiling
 pub fn gxhash0_64(input: &[u8], seed: i32) -> u64 {
     unsafe {
         let p = &gxhash::<0>(input, seed) as *const state as *const u64;
@@ -20,7 +20,7 @@ pub fn gxhash0_64(input: &[u8], seed: i32) -> u64 {
     }
 }
 
-#[inline] // To be disabled when profiling
+#[inline(always)] // To be disabled when profiling
 pub fn gxhash1_32(input: &[u8], seed: i32) -> u32 {
     unsafe {
         let p = &gxhash::<1>(input, seed) as *const state as *const u32;
@@ -28,7 +28,7 @@ pub fn gxhash1_32(input: &[u8], seed: i32) -> u32 {
     }
 }
 
-#[inline] // To be disabled when profiling
+#[inline(always)] // To be disabled when profiling
 pub fn gxhash1_64(input: &[u8], seed: i32) -> u64 {
     unsafe {
         let p = &gxhash::<1>(input, seed) as *const state as *const u64;
@@ -36,90 +36,104 @@ pub fn gxhash1_64(input: &[u8], seed: i32) -> u64 {
     }
 }
 
-#[inline]
+const VECTOR_SIZE: isize = std::mem::size_of::<state>() as isize;
+
+
+#[inline(always)]
+unsafe fn compress<const N: usize>(a: state, b: state) -> state {
+    match N {
+        0 => compress_0(a, b),
+        1 => compress_1(a, b),
+        _ => compress_1(a, b)
+    }
+}
+
+#[inline(always)]
 fn gxhash<const N: usize>(input: &[u8], seed: i32) -> state {
     unsafe {
-        const VECTOR_SIZE: isize = std::mem::size_of::<state>() as isize;
-        
         let len: isize = input.len() as isize;
 
         let p = input.as_ptr() as *const i8;
-        let mut v = p as *const state;
+        let v = p as *const state;
 
-        // Quick exit
-        if len <= VECTOR_SIZE {
-            let partial_vector = get_partial(v, len);
-            return finalize(partial_vector, seed);
-        }
-
-        let mut end_address: usize;
-        let mut remaining_blocks_count: isize = len / VECTOR_SIZE;
-        let mut hash_vector: state = create_empty();
-
-        // Choose compression function depending on version.
-        // Lower is faster, higher is more collision resistant.
-        let c = match N {
-            0 => compress_0,
-            1 => compress_1,
-            _ => compress_1
+        let hash_vector = if len <= 16 {
+            get_partial(v, len)
+        } else if len < 128 {
+            gxhash_process_1::<N>(v, create_empty(), len)
+        } else {
+            gxhash_process_8::<N>(v, create_empty(), len)
         };
-
-        macro_rules! load_unaligned {
-            ($($var:ident),+) => {
-                $(
-                    #[allow(unused_mut)]
-                    let mut $var = load_unaligned(v);
-                    v = v.offset(1);
-                )+
-            };
-        }
-
-        const UNROLL_FACTOR: isize = 8;
-        if len >= VECTOR_SIZE * UNROLL_FACTOR {
-
-            let unrollable_blocks_count: isize = (len / (VECTOR_SIZE * UNROLL_FACTOR)) * UNROLL_FACTOR; 
-            end_address = v.offset(unrollable_blocks_count) as usize;
-    
-            load_unaligned!(s0, s1, s2, s3, s4, s5, s6, s7);
- 
-            while (v as usize) < end_address {
-                
-                load_unaligned!(v0, v1, v2, v3, v4, v5, v6, v7);
-
-                prefetch(v);
-
-                s0 = c(s0, v0);
-                s1 = c(s1, v1);
-                s2 = c(s2, v2);
-                s3 = c(s3, v3);
-                s4 = c(s4, v4);
-                s5 = c(s5, v5);
-                s6 = c(s6, v6);
-                s7 = c(s7, v7);
-            }
-        
-            let a = c(c(s0, s1), c(s2, s3));
-            let b = c(c(s4, s5), c(s6, s7));
-            hash_vector = c(a, b);
-
-            remaining_blocks_count -= unrollable_blocks_count;
-        }
-
-        end_address = v.offset(remaining_blocks_count) as usize;
-
-        while likely((v as usize) < end_address) {
-            load_unaligned!(v0);
-            hash_vector = c(hash_vector, v0);
-        }
-
-        let remaining_bytes = len & (VECTOR_SIZE - 1);
-        if likely(remaining_bytes > 0) {
-            let partial_vector = get_partial(v, remaining_bytes);
-            hash_vector = c(hash_vector, partial_vector);
-        }
 
         finalize(hash_vector, seed)
     }
+}
+
+macro_rules! load_unaligned {
+    ($ptr:ident, $($var:ident),+) => {
+        $(
+            #[allow(unused_mut)]
+            let mut $var = load_unaligned($ptr);
+            $ptr = $ptr.offset(1);
+        )+
+    };
+}
+
+#[inline(always)]
+unsafe fn gxhash_process_8<const N: usize>(mut v: *const state, hash_vector: state, remaining_bytes: isize) -> state {
+
+    const UNROLL_FACTOR: isize = 8;
+
+    let unrollable_blocks_count: isize = remaining_bytes / (VECTOR_SIZE * UNROLL_FACTOR) * UNROLL_FACTOR; 
+    let end_address = v.offset(unrollable_blocks_count as isize) as usize;
+
+    load_unaligned!(v, s0, s1, s2, s3, s4, s5, s6, s7);
+
+    while (v as usize) < end_address {
+        
+        load_unaligned!(v, v0, v1, v2, v3, v4, v5, v6, v7);
+
+        prefetch(v);
+
+        s0 = compress::<N>(s0, v0);
+        s1 = compress::<N>(s1, v1);
+        s2 = compress::<N>(s2, v2);
+        s3 = compress::<N>(s3, v3);
+        s4 = compress::<N>(s4, v4);
+        s5 = compress::<N>(s5, v5);
+        s6 = compress::<N>(s6, v6);
+        s7 = compress::<N>(s7, v7);
+    }
+
+    let a = compress::<N>(compress::<N>(s0, s1), compress::<N>(s2, s3));
+    let b = compress::<N>(compress::<N>(s4, s5), compress::<N>(s6, s7));
+    let hash_vector = compress::<N>(hash_vector, compress::<N>(a, b));
+
+    gxhash_process_1::<N>(v, hash_vector, remaining_bytes - unrollable_blocks_count * VECTOR_SIZE)
+}
+
+#[inline(always)]
+unsafe fn gxhash_process_1<const N: usize>(mut v: *const state, hash_vector: state, remaining_bytes: isize) -> state {
+    
+    let end_address = v.offset((remaining_bytes / VECTOR_SIZE) as isize) as usize;
+
+    let mut hash_vector = hash_vector;
+    while (v as usize) < end_address {
+        load_unaligned!(v, v0);
+        hash_vector = compress::<N>(hash_vector, v0);
+    }
+
+    let remaining_bytes = remaining_bytes & (VECTOR_SIZE - 1);
+    if remaining_bytes > 0 {
+        hash_vector = gxhash_process_last::<N>(v, hash_vector, remaining_bytes);
+    }
+    hash_vector
+}
+
+#[inline(always)]
+unsafe fn gxhash_process_last<const N: usize>(v: *const state, hash_vector: state, remaining_bytes: isize) -> state {
+
+    let partial_vector = get_partial(v, remaining_bytes);
+    compress::<N>(hash_vector, partial_vector)
 }
 
 #[cfg(test)]
