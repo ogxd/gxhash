24922ab4802220a3d15bae4400f0a30f8caa1143
Olivier Giniaux <oginiaux@gmail.com>
2023-10-29 02:47:18 +0200

    Update FFI lib

--- ffi/src/lib.rs
+++ ffi/src/lib.rs
@@ -1,25 +1,13 @@
 use core::slice;
 
 #[no_mangle]
-pub unsafe extern "C" fn gxhash0_32(buf: *const (), len: usize, seed: i32) -> u32 {
+pub unsafe extern "C" fn gxhash32(buf: *const (), len: usize, seed: i32) -> u32 {
     let data: &[u8] = slice::from_raw_parts(buf as *const u8, len);
-    gxhash::gxhash0_32(data, seed)
+    gxhash::gxhash32(data, seed)
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn gxhash0_64(buf: *const (), len: usize, seed: i32) -> u64 {
+pub unsafe extern "C" fn gxhash64(buf: *const (), len: usize, seed: i32) -> u64 {
     let data: &[u8] = slice::from_raw_parts(buf as *const u8, len);
-    gxhash::gxhash0_64(data, seed)
-}
-
-#[no_mangle]
-pub unsafe extern "C" fn gxhash1_32(buf: *const (), len: usize, seed: i32) -> u32 {
-    let data: &[u8] = slice::from_raw_parts(buf as *const u8, len);
-    gxhash::gxhash1_32(data, seed)
-}
-
-#[no_mangle]
-pub unsafe extern "C" fn gxhash1_64(buf: *const (), len: usize, seed: i32) -> u64 {
-    let data: &[u8] = slice::from_raw_parts(buf as *const u8, len);
-    gxhash::gxhash1_64(data, seed)
+    gxhash::gxhash64(data, seed)
 }
