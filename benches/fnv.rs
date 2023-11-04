0cec929ddc388abd57286b71c216f21845eaa5e5
Olivier Giniaux <oginiaux@smartadserver.com>
2023-11-04 01:36:17 +0100

    Add custom throughput benchmark

--- benches/fnv.rs
+++ benches/fnv.rs
@@ -2,8 +2,8 @@ const INITIAL_STATE: u64 = 0xcbf29ce484222325;
 const PRIME: u64 = 0x100000001b3;
 
 #[inline]
-pub const fn fnv_hash(bytes: &[u8]) -> u64 {
-    let mut hash = INITIAL_STATE;
+pub const fn fnv_hash(bytes: &[u8], seed: u64) -> u64 {
+    let mut hash = seed + INITIAL_STATE;
     let mut i = 0;
     while i < bytes.len() {
         hash = hash ^ (bytes[i] as u64);
