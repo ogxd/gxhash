655212d8ca8a4230202a157adf6a177d2817d8d0
Olivier Giniaux <oginiaux@smartadserver.com>
2023-11-29 23:14:10 +0100

    Try remove stdsimd unstable requirement

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 // Feature 'avx2' currently requires unstable 'stdsimd'
-#![cfg_attr(all(target_arch = "x86_64"), feature(stdsimd))]
+//#![cfg_attr(all(target_arch = "x86_64"), feature(stdsimd))]
 
 #[rustfmt::skip]
 mod gxhash;
