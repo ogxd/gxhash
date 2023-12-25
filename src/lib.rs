cabf31a899b0bd98f865c18fcd7d0c8883da522e
Moritz Mœller <virtualritz@protonmail.com>
2023-12-25 22:02:25 +0100

    Add no `no_std` support

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,12 @@
+#![cfg_attr(not(feature = "std"), no_std)]
 // Hybrid SIMD width usage currently requires unstable 'stdsimd'
 #![cfg_attr(hybrid, feature(stdsimd))]
 
 #[rustfmt::skip]
 mod gxhash;
-mod hasher;
-
 pub use crate::gxhash::*;
+
+#[cfg(feature = "std")]
+mod hasher;
+#[cfg(feature = "std")]
 pub use crate::hasher::*;
