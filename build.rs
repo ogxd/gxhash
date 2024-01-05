f42b0258f28b423f7e787d533add4285fd208377
Olivier Giniaux <oginiaux@gmail.com>
2024-01-05 21:27:12 +0100

    Make build time check failure message more explicit

--- build.rs
+++ build.rs
@@ -3,8 +3,8 @@ use rustc_version::{version_meta, Channel};
 
 fn main() {
 
-    if !has_hardware_support() {
-        panic!("Hardware is not supported");
+    if let Err(e) = check_hardware_support() {
+        panic!("Gxhash build failed: {}", e);
     }
 
     if version_meta().unwrap().channel == Channel::Nightly
@@ -16,11 +16,26 @@ fn main() {
 }
 
 #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
-fn has_hardware_support() -> bool {
-    std::is_x86_feature_detected!("aes") && std::is_x86_feature_detected!("sse2")
+fn has_hardware_support() -> Result<(), Box<dyn std::error::Error>> {
+    if !std::is_x86_feature_detected!("aes") {
+        return Result::Err("CPU feature 'aes' is required")?;
+    }
+    if !std::is_x86_feature_detected!("sse2") {
+        return Result::Err("CPU feature 'sse2' is required")?;
+    }
+    return Ok(());
 }
 
 #[cfg(not(any(target_arch = "x86", target_arch = "x86_64")))]
-fn has_hardware_support() -> bool {
-    cfg!(all(any(target_arch = "arm", target_arch = "aarch64"), target_feature = "aes", target_feature = "neon"))
+fn check_hardware_support() -> Result<(), Box<dyn std::error::Error>> {
+    if cfg!(any(target_arch = "arm", target_arch = "aarch64")) {
+        if cfg!(not(target_feature = "aes")) {
+            return Result::Err("CPU feature 'aes' is required")?;
+        }
+        if cfg!(not(target_feature = "neon")) {
+            return Result::Err("CPU feature 'neon' is required")?;
+        }
+        return Ok(());
+    }
+    return Result::Err(format!("Target arch '{}' is not supported", std::env::consts::ARCH))?;
 }
