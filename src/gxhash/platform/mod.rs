580ccb85cfdda5dfaece2ebb97719a13a61b17b2
Olivier Giniaux <oginiaux@gmail.com>
2024-01-06 22:24:13 +0100

    Add cargo bench

--- src/gxhash/platform/mod.rs
+++ src/gxhash/platform/mod.rs
@@ -1,8 +1,8 @@
-#[cfg(all(any(target_arch = "arm", target_arch = "aarch64"), target_feature = "aes", target_feature = "neon"))]
+#[cfg(all(any(target_arch = "arm", target_arch = "aarch64"), target_feature = "aes"))]
 #[path = "arm.rs"]
 mod platform;
 
-#[cfg(all(any(target_arch = "x86", target_arch = "x86_64"), target_feature = "aes", target_feature = "sse2"))]
+#[cfg(all(any(target_arch = "x86", target_arch = "x86_64"), target_feature = "aes"))]
 #[path = "x86.rs"]
 mod platform;
 
