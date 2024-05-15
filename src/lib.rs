a61616ec26fff9d0800697ea1ce380e3df66c29c
Olivier Giniaux <oginiaux@gmail.com>
2024-05-15 23:33:12 +0200

    Fix x86 hybrid build since rust 1.79 (#68)
    * Fix x86 hybrid build since rust update
    
    * Try remove stdsimd
    
    * Display version
    
    * Try github macos runner
    
    * Set version to 3.2.0
    
    * Test rustc 1.72 with hybrid
    
    * Add rust version compat check
    
    * Try use patch in v
    
    * Another test
    
    * Disable fail fast
    
    * Fix hybrid
    
    * Fix missing cpu feature
    
    * No hybrid
    
    * Workflow dispatch

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 // Hybrid SIMD width usage currently requires unstable 'stdsimd'
-#![cfg_attr(hybrid, feature(stdsimd))]
+#![cfg_attr(hybrid, feature(stdarch_x86_avx512))]
 
 #[rustfmt::skip]
 mod gxhash;
