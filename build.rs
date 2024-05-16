679325731fd2092e7e1d48290c29e5839e23aeba
Olivier Giniaux <oginiaux@smartadserver.com>
2024-05-16 23:46:20 +0200

    Use compile_error macro to replace build script

--- build.rs
+++ build.rs
@@ -2,18 +2,5 @@ extern crate rustc_version;
 use rustc_version::{version_meta, Channel};
 
 fn main() {
-    // When conditions permits, enable hybrid feature to leverage wider intrinsics for even more throughput
-    if version_meta().unwrap().channel == Channel::Nightly
-    && cfg!(target_arch = "x86_64")
-    && cfg!(target_feature = "avx2")
-    && cfg!(target_feature = "vaes") {
-        println!("cargo:rustc-cfg=hybrid");
-    }
 
-    // If not cross compiling, make sure the aes feature is available
-    if std::env::var("HOST").unwrap_or_default() == std::env::var("TARGET").unwrap_or_default()
-    && cfg!(not(target_feature = "aes")) {
-        panic!("| GxHash requires target-feature 'aes' to be enabled.\n\
-        | Build with RUSTFLAGS=\"-C target-cpu=native\" or RUSTFLAGS=\"-C target-feature=+aes\" to enable.");
-    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,12 @@
 // Hybrid SIMD width usage currently requires unstable 'stdsimd'
 #![cfg_attr(hybrid, feature(stdarch_x86_avx512))]
 
+#[cfg(all(feature = "hybrid", not(any(target_arch = "x86_64", target_feature = "avx2", target_feature = "vaes"))))]
+compile_error!{"Hybrid feature is only available on x86 processors with avx2 and vaes intrinsics."}
+
+#[cfg(not(target_feature = "aes"))]
+compile_error!{"Gxhash requires aes intrinsics. Make sure the processor supports it and build with RUSTFLAGS=\"-C target-cpu=native\" or RUSTFLAGS=\"-C target-feature=+aes\"."}
+
 #[rustfmt::skip]
 mod gxhash;
 mod hasher;
