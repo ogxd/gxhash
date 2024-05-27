7f591bbf2c42a1b7a8e1f8d655390c5e3d715711
Olivier Giniaux <oginiaux@smartadserver.com>
2024-05-27 09:45:52 +0200

    Try use all flags in x compile CI

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 // Hybrid SIMD width usage currently requires unstable 'stdsimd'
 #![cfg_attr(feature = "hybrid", feature(stdarch_x86_avx512))]
 
-#[cfg(all(feature = "hybrid", not(any(target_arch = "x86_64", target_feature = "avx2", target_feature = "vaes"))))]
+#[cfg(all(feature = "hybrid", not(any(target_arch = "x86_64", target_feature = "aes", target_feature = "vaes", target_feature = "avx2"))))]
 compile_error!{"Hybrid feature is only available on x86 processors with avx2 and vaes intrinsics."}
 
 #[cfg(not(target_feature = "aes"))]
