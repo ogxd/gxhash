b85cfb9b158aa8d818d22d4903e34d2a4a1d4f78
Olivier Giniaux <oginiaux@gmail.com>
2024-05-27 14:31:01 +0200

    Use compile_error macro to replace build script (#71)
    * Use compile_error macro to replace build script
    
    * Update rustc in bench runners
    
    * Add CI to test cross compilation
    
    * Add more targets
    
    * Prepare for merge
    
    * Also build from macos arm
    
    * Use nightly rust for hybrid
    
    * Fix more cross compilation cases
    
    * Install toolchain if needed
    
    * Add last target back, even if not working
    
    * Check if changes fixes doctest
    
    * Try use rustdocflags
    
    * Reuse std::hash::RandomState to get rid of rand dependency
    
    * Try set rustflags in x compile CI
    
    * Try use all flags in x compile CI
    
    * Fine tune cross compilation ci

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,11 @@
 // Hybrid SIMD width usage currently requires unstable 'stdsimd'
-#![cfg_attr(hybrid, feature(stdarch_x86_avx512))]
+#![cfg_attr(feature = "hybrid", feature(stdarch_x86_avx512))]
+
+#[cfg(all(feature = "hybrid", not(any(target_arch = "x86_64", target_feature = "aes", target_feature = "vaes", target_feature = "avx2"))))]
+compile_error!{"Hybrid feature is only available on x86 processors with avx2 and vaes intrinsics."}
+
+#[cfg(not(target_feature = "aes"))]
+compile_error!{"Gxhash requires aes intrinsics. Make sure the processor supports it and build with RUSTFLAGS=\"-C target-cpu=native\" or RUSTFLAGS=\"-C target-feature=+aes\"."}
 
 #[rustfmt::skip]
 mod gxhash;
