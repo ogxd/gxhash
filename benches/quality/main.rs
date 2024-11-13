2a93356dcde943f35b198d5388cfab6e6deba6e4
Olivier Giniaux <oginiaux@gmail.com>
2024-11-14 00:26:45 +0100

    Add foldhash to bench

--- benches/quality/main.rs
+++ benches/quality/main.rs
@@ -3,12 +3,16 @@ use rand::Rng;
 use criterion::black_box;
 
 fn main() {
+    // Passing hash functions ✅
     bench_hasher_quality::<gxhash::GxBuildHasher>("GxHash");
     bench_hasher_quality::<std::collections::hash_map::RandomState>("Default");
     bench_hasher_quality::<twox_hash::xxh3::RandomHashBuilder64>("XxHash (XXH3)");
     bench_hasher_quality::<ahash::RandomState>("AHash");
     bench_hasher_quality::<t1ha::T1haBuildHasher>("T1ha");
+
+    // Not passing hash functions ❌
     bench_hasher_quality::<fnv::FnvBuildHasher>("FNV-1a");
+    bench_hasher_quality::<foldhash::quality::RandomState>("FoldHash");
 }
 
 macro_rules! check {
