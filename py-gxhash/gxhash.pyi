8000673028cad39076488528cc662767ed63159b
winstxnhdw <winstxnhdw@gmail.com>
2024-11-17 06:30:09 +0000

    feat: remove async variants

--- py-gxhash/gxhash.pyi
+++ py-gxhash/gxhash.pyi
@@ -28,7 +28,6 @@ def gxhash32(input_bytes: bytes, seed: int) -> int:
     ```
     """
 
-
 def gxhash32_nogil(input_bytes: bytes, seed: int) -> int:
     """
     Summary
@@ -58,42 +57,6 @@ def gxhash32_nogil(input_bytes: bytes, seed: int) -> int:
     print(f"Hash is {gxhash.gxhash32_nogil(input_bytes, seed)}!")
     ```
     """
-
-
-async def gxhash32_async(input_bytes: bytes, seed: int) -> int:
-    """
-    Summary
-    -------
-    hashes an arbitrary stream of bytes to an u32 asynchronously in a Rust thread
-
-
-    Parameters
-    ----------
-    input_bytes (bytes): input bytes to hash
-
-    seed (int): seed for the hash function
-
-
-    Returns
-    -------
-    hash (int): u32 hash of the input bytes
-
-
-    Example
-    -------
-    ```python
-    import gxhash
-    import asyncio
-
-    async def main():
-        input_bytes =  bytes([42] * 1000)
-        seed = 1234
-        print(f"Hash is {await gxhash.gxhash32_async(input_bytes, seed)}!")
-
-    asyncio.run(main())
-    ```
-    """
-
 
 def gxhash64(input_bytes: bytes, seed: int) -> int:
     """
@@ -125,7 +88,6 @@ def gxhash64(input_bytes: bytes, seed: int) -> int:
     ```
     """
 
-
 def gxhash64_nogil(input_bytes: bytes, seed: int) -> int:
     """
     Summary
@@ -155,42 +117,6 @@ def gxhash64_nogil(input_bytes: bytes, seed: int) -> int:
     print(f"Hash is {gxhash.gxhash64_nogil(input_bytes, seed)}!")
     ```
     """
-
-
-async def gxhash64_async(input_bytes: bytes, seed: int) -> int:
-    """
-    Summary
-    -------
-    hashes an arbitrary stream of bytes to an u64 asynchronously in a Rust thread
-
-
-    Parameters
-    ----------
-    input_bytes (bytes): input bytes to hash
-
-    seed (int): seed for the hash function
-
-
-    Returns
-    -------
-    hash (int): u64 hash of the input bytes
-
-
-    Example
-    -------
-    ```python
-    import gxhash
-    import asyncio
-
-    async def main():
-        input_bytes =  bytes([42] * 1000)
-        seed = 1234
-        print(f"Hash is {await gxhash.gxhash64_async(input_bytes, seed)}!")
-
-    asyncio.run(main())
-    ```
-    """
-
 
 def gxhash128(input_bytes: bytes, seed: int) -> int:
     """
@@ -222,7 +148,6 @@ def gxhash128(input_bytes: bytes, seed: int) -> int:
     ```
     """
 
-
 def gxhash128_nogil(input_bytes: bytes, seed: int) -> int:
     """
     Summary
@@ -250,40 +175,5 @@ def gxhash128_nogil(input_bytes: bytes, seed: int) -> int:
     input_bytes =  bytes([42] * 1000)
     seed = 1234
     print(f"Hash is {gxhash.gxhash128_nogil(input_bytes, seed)}!")
-    ```
-    """
-
-
-async def gxhash128_async(input_bytes: bytes, seed: int) -> int:
-    """
-    Summary
-    -------
-    hashes an arbitrary stream of bytes to an u128 asynchronously in a Rust thread
-
-
-    Parameters
-    ----------
-    input_bytes (bytes): input bytes to hash
-
-    seed (int): seed for the hash function
-
-
-    Returns
-    -------
-    hash (int): u128 hash of the input bytes
-
-
-    Example
-    -------
-    ```python
-    import gxhash
-    import asyncio
-
-    async def main():
-        input_bytes =  bytes([42] * 1000)
-        seed = 1234
-        print(f"Hash is {await gxhash.gxhash128_async(input_bytes, seed)}!")
-
-    asyncio.run(main())
     ```
     """
--- py-gxhash/src/lib.rs
+++ py-gxhash/src/lib.rs
@@ -14,20 +14,6 @@ fn gxhash32_nogil(py: Python, input_bytes: &[u8], seed: i64) -> PyResult<u32> {
 }
 
 #[pyfunction]
-fn gxhash32_async<'p>(py: Python<'p>, input_bytes: &'p [u8], seed: i64) -> PyResult<Bound<'p, PyAny>> {
-    let input_bytes_clone = input_bytes.to_vec();
-
-    future_into_py(py, async move {
-        let result = Python::with_gil(|py| py.allow_threads(|| spawn_blocking(move || gxhash::gxhash32(&input_bytes_clone, seed)))).await;
-
-        match result {
-            Ok(result) => Ok(result),
-            Err(e) => Err(PyRuntimeError::new_err(format!("Task failed: {:?}", e))),
-        }
-    })
-}
-
-#[pyfunction]
 fn gxhash64(input_bytes: &[u8], seed: i64) -> PyResult<u64> {
     Ok(gxhash::gxhash64(input_bytes, seed))
 }
@@ -35,20 +21,6 @@ fn gxhash64(input_bytes: &[u8], seed: i64) -> PyResult<u64> {
 #[pyfunction]
 fn gxhash64_nogil(py: Python, input_bytes: &[u8], seed: i64) -> PyResult<u64> {
     py.allow_threads(|| Ok(gxhash::gxhash64(input_bytes, seed)))
-}
-
-#[pyfunction]
-fn gxhash64_async<'p>(py: Python<'p>, input_bytes: &'p [u8], seed: i64) -> PyResult<Bound<'p, PyAny>> {
-    let input_bytes_clone = input_bytes.to_vec();
-
-    future_into_py(py, async move {
-        let result = Python::with_gil(|py| py.allow_threads(|| spawn_blocking(move || gxhash::gxhash64(&input_bytes_clone, seed)))).await;
-
-        match result {
-            Ok(result) => Ok(result),
-            Err(e) => Err(PyRuntimeError::new_err(format!("Task failed: {:?}", e))),
-        }
-    })
 }
 
 #[pyfunction]
@@ -61,30 +33,13 @@ fn gxhash128_nogil(py: Python, input_bytes: &[u8], seed: i64) -> PyResult<u128> 
     py.allow_threads(|| Ok(gxhash::gxhash128(input_bytes, seed)))
 }
 
-#[pyfunction]
-fn gxhash128_async<'p>(py: Python<'p>, input_bytes: &'p [u8], seed: i64) -> PyResult<Bound<'p, PyAny>> {
-    let input_bytes_clone = input_bytes.to_vec();
-
-    future_into_py(py, async move {
-        let result = Python::with_gil(|py| py.allow_threads(|| spawn_blocking(move || gxhash::gxhash128(&input_bytes_clone, seed)))).await;
-
-        match result {
-            Ok(result) => Ok(result),
-            Err(e) => Err(PyRuntimeError::new_err(format!("Task failed: {:?}", e))),
-        }
-    })
-}
-
 #[pymodule(name = "gxhash")]
 fn pygxhash(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(gxhash32, m)?)?;
     m.add_function(wrap_pyfunction!(gxhash32_nogil, m)?)?;
-    m.add_function(wrap_pyfunction!(gxhash32_async, m)?)?;
     m.add_function(wrap_pyfunction!(gxhash64, m)?)?;
     m.add_function(wrap_pyfunction!(gxhash64_nogil, m)?)?;
-    m.add_function(wrap_pyfunction!(gxhash64_async, m)?)?;
     m.add_function(wrap_pyfunction!(gxhash128, m)?)?;
     m.add_function(wrap_pyfunction!(gxhash128_nogil, m)?)?;
-    m.add_function(wrap_pyfunction!(gxhash128_async, m)?)?;
     Ok(())
 }
